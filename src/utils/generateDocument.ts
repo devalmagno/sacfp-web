b1094cc00591ead085041bde13f6f298821a45f1
devalmagno <devalmagno@gmail.com>
2023-03-03 16:02:37 -0300

    Generate Pointsheet Class Row's Using Workload as Range

--- src/utils/generateDocument.ts
+++ src/utils/generateDocument.ts
@@ -26,44 +26,42 @@ const generateDocument = (props: Props) => {
 
     schoolDays.forEach(e => number += e.schedules.length);
 
-    console.log(schoolDays);
+    loadFile(
+        '/pointsheets/pointsheet_model.docx',
+        (error, content) => {
+            if (error) throw error;
 
-    // loadFile(
-    //     '/pointsheets/pointsheet_model.docx',
-    //     (error, content) => {
-    //         if (error) throw error;
+            const zip = new PizZip(content);
+            const doc = new Docxtemplater(zip, {
+                paragraphLoop: true,
+                linebreaks: true,
+            });
 
-    //         const zip = new PizZip(content);
-    //         const doc = new Docxtemplater(zip, {
-    //             paragraphLoop: true,
-    //             linebreaks: true,
-    //         });
+            doc.setData({
+                course: props.pointsheet.sheet.course,
+                semester: semesterNumber,
+                year,
+                name: props.pointsheet.name,
+                masp: props.pointsheet.masp,
+                discipline: props.pointsheet.sheet.discipline,
+                period: props.pointsheet.sheet.period,
+                workload: props.pointsheet.sheet.workload,
+                schoolDays,
+            });
 
-    //         doc.setData({
-    //             course: props.pointsheet.sheet.course,
-    //             semester: semesterNumber,
-    //             year,
-    //             name: props.pointsheet.name,
-    //             masp: props.pointsheet.masp,
-    //             discipline: props.pointsheet.sheet.discipline,
-    //             period: props.pointsheet.sheet.period,
-    //             workload: props.pointsheet.sheet.workload,
-    //             schoolDays,
-    //         });
+            try {
+                doc.render();
+            } catch (e) {
+                console.log(e);
+            }
 
-    //         try {
-    //             doc.render();
-    //         } catch (e) {
-    //             console.log(e);
-    //         }
-
-    //         const out = doc.getZip().generate({
-    //             type: 'blob',
-    //             MimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    //         });
-    //         saveAs(out, `${props.pointsheet.sheet.discipline}_${props.pointsheet.name}.docx`);
-    //     }
-    // )
+            const out = doc.getZip().generate({
+                type: 'blob',
+                MimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            });
+            saveAs(out, `${props.pointsheet.sheet.discipline}_${props.pointsheet.name}.docx`);
+        }
+    )
 }
 
 export default generateDocument;
